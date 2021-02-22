8ebb6f4d0b062e3b49e485a5855b6447b797faa2
primitivorm <cibercafe_montero@hotmail.com>
2021-02-22 19:11:59 +0000

    se agrega exit(EXIT_FAILURE); a latparse.c

--- src/latparse.c
+++ src/latparse.c
@@ -2594,6 +2594,7 @@ const char *s) {
   if(!parse_silent){
       fprintf(stderr, LAT_ERROR_FMT, yylloc_param->file_name,
         yylloc_param->first_line, yylloc_param->first_column,  s);
+      exit(EXIT_FAILURE);
   }
   return 0;
 }
